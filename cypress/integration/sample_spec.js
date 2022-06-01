afbfb9136e2f8f89198eb65f3eae26c9f3fd0804
Carlos Eduardo <cpwaldow@gmail.com>
2022-05-31 22:20:33 -0300

    visitando uma url

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,4 +2,7 @@ describe('My first test', () => {
   it('Does not much!', () => {
     expect(true).to.equal(true);
   });
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('https://github.com/cpwaldow/cypress');
+  });
 });
